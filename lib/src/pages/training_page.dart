f22d87563fde27a4d7787752aba2c523c416a294
kaanyagci <kaan.yagci@clique.paris>
2020-09-16 00:52:05 +0200

    Typo fixed in imports

--- lib/src/pages/training_page.dart
+++ lib/src/pages/training_page.dart
@@ -1,7 +1,7 @@
 import 'package:arithmic/src/organisms/training_page_content.dart';
 import 'package:arithmic/src/services/settings_button_service.dart';
 import 'package:arithmic/src/templates/header.dart';
-import 'package:arithmic/src/templates/sliding_setting_menu_page_template.dart';
+import 'package:arithmic/src/templates/sliding_settings_menu_page_template.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
