ed2b5015b77a1d715cc1fe964d4c51db9f3db38d
kaanyagci <kaan.yagci@clique.paris>
2020-09-16 00:49:42 +0200

    Static routeName added in HomePage

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,10 +1,13 @@
+import 'package:arithmic/src/organisms/home_page_content.dart';
 import 'package:arithmic/src/services/settings_button_service.dart';
 import 'package:arithmic/src/templates/header.dart';
-import 'package:arithmic/src/templates/home_page_template.dart';
+import 'package:arithmic/src/templates/sliding_setting_menu_page_template.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
+  static String routeName = '/home';
+
   const HomePage({
     Key key,
   });
@@ -14,6 +17,6 @@ class HomePage extends StatelessWidget {
         Provider.of<SettingsButtonService>(context, listen: true);
     return Scaffold(
         appBar: _ss.isOpen ? null : Header(height: 100),
-        body: HomePageTemplate());
+        body: SlidingSettingsMenuPageTemplate(child: HomePageContent()));
   }
 }
