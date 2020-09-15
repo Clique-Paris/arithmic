34fc758ac8f399c3feef28b15aa78fa17a7b3141
kaanyagci <kaan.yagci@clique.paris>
2020-09-15 10:21:47 +0200

    Themes updated with visual density updated

--- lib/src/themes.dart
+++ lib/src/themes.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 abstract class Themes {
-  static final ThemeData light =
-      ThemeData(brightness: Brightness.light, primaryColor: Colors.purple);
+  static final ThemeData light = ThemeData(
+      brightness: Brightness.light,
+      primaryColor: Colors.purple,
+      visualDensity: VisualDensity.adaptivePlatformDensity);
 
-  static final ThemeData dark =
-      ThemeData(brightness: Brightness.dark, primaryColor: Colors.yellow);
+  static final ThemeData dark = ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: Colors.yellow,
+      visualDensity: VisualDensity.adaptivePlatformDensity);
 }
