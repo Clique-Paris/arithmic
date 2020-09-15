aed562db8a71d189911fdf96de2b464c0f8c6277
kaanyagci <kaan.yagci@clique.paris>
2020-09-16 00:49:25 +0200

    Router updated with TrainingPage and static routeNames

--- lib/src/utils/router.dart
+++ lib/src/utils/router.dart
@@ -1,8 +1,10 @@
 import 'package:arithmic/src/pages/home_page.dart';
+import 'package:arithmic/src/pages/training_page.dart';
 import 'package:flutter/widgets.dart';
 
 abstract class Router {
   static final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
-    '/home': (BuildContext context) => HomePage(),
+    HomePage.routeName: (BuildContext context) => HomePage(),
+    TrainingPage.routeName: (BuildContext context) => TrainingPage(),
   };
 }
