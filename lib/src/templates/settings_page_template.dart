a8c73b07ae0d09442e9dc010f24d69be7cb130a3
kaanyagci <kaan.yagci@clique.paris>
2020-09-16 09:58:33 +0200

    Print and usused statements are removed in settings_page_Template

--- lib/src/templates/settings_page_template.dart
+++ lib/src/templates/settings_page_template.dart
@@ -10,22 +10,9 @@ class SettingsPageTemplate extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Offset _posStart, _posEnd;
-    Size _deviceSize = MediaQuery.of(context).size;
-    SettingsButtonService _settingsButtonService =
-        Provider.of<SettingsButtonService>(context, listen: false);
     return GestureDetector(
-        onVerticalDragStart: (DragStartDetails details) {
-          // _posStart = details.globalPosition;R
-        },
         onVerticalDragUpdate: (DragUpdateDetails details) {
-          _posEnd = details.globalPosition;
           onDrag(details.delta.dy);
-        },
-        onVerticalDragEnd: (DragEndDetails details) {
-          // if (((_posEnd.dy - _posStart.dy) / _deviceSize.height) > 0.5) {
-          //   _settingsButtonService.onClick();
-          // }
         },
         child: Container(
           color: Colors.red,
