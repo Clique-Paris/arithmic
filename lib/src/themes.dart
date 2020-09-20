836289276222071f82f22f6ca0718ad0ec86bb59
kaanyagci <kaan.yagci@clique.paris>
2020-09-20 13:04:22 +0200

    Themes definitions updated

--- lib/src/themes.dart
+++ lib/src/themes.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 
 abstract class Themes {
   static final ThemeData light = ThemeData(
-      brightness: Brightness.light,
-      primaryColor: Colors.purple,
-      visualDensity: VisualDensity.adaptivePlatformDensity);
+    brightness: Brightness.light,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    scaffoldBackgroundColor: Colors.white,
+    fontFamily: 'RobotoMono',
+  );
 
   static final ThemeData dark = ThemeData(
-      brightness: Brightness.dark,
-      primaryColor: Colors.yellow,
-      visualDensity: VisualDensity.adaptivePlatformDensity);
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: Colors.grey[850],
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    fontFamily: 'RobotoMono',
+  );
 }
