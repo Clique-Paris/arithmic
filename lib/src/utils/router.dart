174b07358ee3cd26b184eae1bb2a84288a70299e
kaanyagci <kaan.yagci@clique.paris>
2020-09-20 20:10:16 +0200

    AlgebraPage added to router

--- lib/src/utils/router.dart
+++ lib/src/utils/router.dart
@@ -1,3 +1,4 @@
+import 'package:arithmic/src/pages/algebra_page.dart';
 import 'package:arithmic/src/pages/home_page.dart';
 import 'package:arithmic/src/pages/training_page.dart';
 import 'package:flutter/widgets.dart';
@@ -6,5 +7,6 @@ abstract class Router {
   static final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
     HomePage.routeName: (BuildContext context) => HomePage(),
     TrainingPage.routeName: (BuildContext context) => TrainingPage(),
+    AlgebraPage.routeName: (BuildContext context) => AlgebraPage(),
   };
 }
