7aa75c3627cf635bc374f1dd9d6cfe65de62d37c
kaanyagci <kaan.yagci@clique.paris>
2020-09-20 13:14:15 +0200

    TrainingPageContent updated with new buttons colors and new flex layout proportions

--- lib/src/organisms/training_page_content.dart
+++ lib/src/organisms/training_page_content.dart
@@ -1,5 +1,6 @@
-import 'package:arithmic/src/atoms/colorful_flat_button.dart';
+import 'package:arithmic/src/molecules/colorful_flat_text_button.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 /// Widget contains all elements of the training page
 class TrainingPageContent extends StatelessWidget {
@@ -12,31 +13,40 @@ class TrainingPageContent extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Center(child: Text("Training subject:")),
-        Column(
-          children: [
-            ColorfulFlatButton(
-              color: Colors.red,
-              text: 'Basics of algebra',
-              onPressed: () {
-                print('Selected basics of algebra');
-              },
-            ),
-            ColorfulFlatButton(
-              color: Colors.purple,
-              text: 'Base arithmetics (a.k.a Radix)',
-              onPressed: () {
-                print('Base arithmetics selected as training subject');
-              },
-            ),
-            ColorfulFlatButton(
-              color: Colors.blue,
-              text: 'Calculus',
-              onPressed: () {
-                print('Calculus selected as training subject');
-              },
-            )
-          ],
+        Expanded(
+            flex: 2,
+            child: Center(
+                child: Text("Training subject:",
+                    style: GoogleFonts.robotoMono(fontSize: 24)))),
+        Expanded(
+          flex: 3,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ColorfulFlatTextButton(
+                color: Colors.red,
+                text: 'Basics of algebra',
+                onPressed: () {
+                  print('Selected basics of algebra');
+                },
+              ),
+              ColorfulFlatTextButton(
+                color: Colors.yellow,
+                text: 'Base arithmetics (a.k.a Radix)',
+                onPressed: () {
+                  print('Base arithmetics selected as training subject');
+                },
+              ),
+              ColorfulFlatTextButton(
+                color: Colors.green,
+                text: 'Calculus',
+                onPressed: () {
+                  print('Calculus selected as training subject');
+                },
+              )
+            ],
+          ),
         )
       ],
     );
