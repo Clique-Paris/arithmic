8e42f4bfc421ab083603d3c552588d3a77d0a995
kaanyagci <kaan.yagci@clique.paris>
2020-09-20 13:13:36 +0200

    HomePageContent updated with dynamic logo

--- lib/src/organisms/home_page_content.dart
+++ lib/src/organisms/home_page_content.dart
@@ -1,5 +1,7 @@
 import 'package:arithmic/src/molecules/home_page_buttons_list.dart';
+import 'package:arithmic/src/services/theme_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 /// widget contains all internal elements of the home page
 class HomePageContent extends StatelessWidget {
@@ -8,11 +10,15 @@ class HomePageContent extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
+    ThemeService _ts = Provider.of<ThemeService>(context, listen: true);
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Center(child: Image.asset('assets/logo.png')),
+        Center(
+            child: Image.asset(_ts.isDarkMode
+                ? 'assets/dark_mode_logo.png'
+                : 'assets/logo.png')),
         HomePageButtonsList(),
       ],
     );
