b2c39cab069ef6aff294cc962b348349b83195cd
kaanyagci <kaan.yagci@clique.paris>
2020-09-20 14:36:01 +0200

    Close button declaration added

---
+++ lib/src/atoms/close_button.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/// description
+class CloseSettingsPageButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  const CloseSettingsPageButton({Key key, @required this.onPressed});
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(icon: Icon(Icons.close), onPressed: onPressed);
+  }
+}
