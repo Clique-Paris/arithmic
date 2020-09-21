910630f84c8baf0e290d4761c3110dceabf573d1
kaanyagci <kaan.yagci@clique.paris>
2020-09-22 00:01:13 +0200

    Training page updated: unused imports removed

--- lib/src/pages/training_page.dart
+++ lib/src/pages/training_page.dart
@@ -1,10 +1,7 @@
 import 'package:arithmic/src/molecules/training_page_buttons_list.dart';
 import 'package:arithmic/src/templates/title_with_button_list_template.dart';
-import 'package:arithmic/src/services/settings_button_service.dart';
-import 'package:arithmic/src/templates/header.dart';
 import 'package:arithmic/src/templates/sliding_settings_menu_page_scaffold_template.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 /// Contains all elements of the training page
 class TrainingPage extends StatelessWidget {
