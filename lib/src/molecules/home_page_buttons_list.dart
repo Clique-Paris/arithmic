b2ca0bdfef3afecd6183fccff68348371ad5fad9
kaanyagci <kaan.yagci@clique.paris>
2020-09-22 00:02:38 +0200

    HomePageButtonsList updated: Session service integrated

--- lib/src/molecules/home_page_buttons_list.dart
+++ lib/src/molecules/home_page_buttons_list.dart
@@ -1,6 +1,9 @@
 import 'package:arithmic/src/molecules/colorful_flat_text_button.dart';
 import 'package:arithmic/src/pages/training_page.dart';
+import 'package:arithmic/src/services/session_service.dart';
+import 'package:arithmic/src/structs/game_mode.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 /// Widget contains all buttons on the home page
 class HomePageButtonsList extends StatelessWidget {
@@ -9,12 +12,14 @@ class HomePageButtonsList extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
+    SessionService _ss = Provider.of<SessionService>(context, listen: true);
     return Column(
       children: [
         ColorfulFlatTextButton(
             color: Colors.red,
             text: 'Training',
             onPressed: () {
+              _ss.currentGameMode = GameMode.training;
               Navigator.pushNamed(context, TrainingPage.routeName);
             }),
         ColorfulFlatTextButton(
