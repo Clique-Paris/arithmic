01e20de7d26d9f9730b185649669d5bc95824eee
kaanyagci <kaan.yagci@clique.paris>
2020-09-21 23:58:47 +0200

    Difficulty enumeration implemented

---
+++ lib/src/structs/dfficulty.dart
@@ -0,0 +1,18 @@
+enum Difficulty { beginner, intermetidate, advanced, challenger }
+
+extension DifficultyResolver on Difficulty {
+  String get value {
+    switch (this) {
+      case Difficulty.beginner:
+        return 'Beginner';
+      case Difficulty.intermetidate:
+        return 'Intermediate';
+      case Difficulty.advanced:
+        return 'Advanced';
+      case Difficulty.challenger:
+        return 'Challenger';
+      default:
+        throw FormatException(this.toString() + 'is not a supported value');
+    }
+  }
+}
