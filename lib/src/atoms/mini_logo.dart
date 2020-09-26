95c2e14c30748a46eb290f531b923e422a07929a
kaanyagci <kaan.yagci@clique.paris>
2020-09-26 20:18:46 +0200

    Mini logo implemented as new atom

---
+++ lib/src/atoms/mini_logo.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+/// Widget contains the definition of the mini arithmic logo
+class MiniLogo extends StatelessWidget {
+  const MiniLogo({
+    Key key,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('assets/logo.png');
+  }
+}
